a97f606cdde8eda2218b30d48c8fcf45459ba25d
mashudalhasan <mashudalhasan554@gmail.com>
2023-04-11 15:42:14 +0600

    added job details page

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -2,38 +2,101 @@ import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 import banner1 from "../assets/All Images/Vector-1.png";
 import banner2 from "../assets/All Images/Vector.png";
+import dollarIcon from "../assets/Icons/Frame.png";
+import locationImage from "../assets/Icons/location.png";
+import jobTitleIcon from "../assets/Icons/title.png";
+import phoneIcon from "../assets/Icons/phone.png";
+import emailIcon from "../assets/Icons/mail.png";
 
 const JobDetails = () => {
-  const { id } = useParams();
-  console.log(id);
-  
+  const { _id } = useParams();
+  //   console.log(id);
+
   const featuredData = useLoaderData();
-//   console.log(featuredData);
-  
+
   const [jobDetails, setJobDetails] = useState({});
 
-  useEffect( () => {
-    const jobData = featuredData.find(feature => feature.id === id);
-    setJobDetails(jobData);
-    console.log(jobData);
-  } , [])
-  console.log(jobDetails);
-  
+  useEffect(() => {
+    if (featuredData) {
+      // Check if featuredData is truthy
+      const jobData = featuredData.find((feature) => feature.id === _id);
+      setJobDetails(jobData);
+    }
+  }, [featuredData, _id]); // Include featuredData and id in the dependencies array
+
+  //   console.log(jobDetails);
+  const {
+    jobTitle,
+    companyName,
+    location,
+    salary,
+    jobDescription,
+    jobResponsibilities,
+    educationalRequirements,
+    experience,
+    Phone,
+    email,
+  } = jobDetails;
+
   return (
-    <div className="my-container">
-      <div className="flex h-40 justify-between items-center">
-        <img src={banner2} alt="" />
+    <div className="mb-32">
+      <div className="flex h-36 justify-between items-center bg-light-purple -mt-20">
+        <img className="w-1/3 md:w-1/4 lg:w-1/6" src={banner2} alt="" />
         <p className="text-2xl font-bold text-center">Job Details</p>
-        <img src={banner1} alt="" />
+        <img className="w-1/3 md:w-1/4 lg:w-1/6" src={banner1} alt="" />
       </div>
-      <div className="mt-32">
-        <p>Coming soon...</p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque enim
-          fugiat, molestias eaque temporibus consequuntur a rem eos, quasi
-          voluptatem nisi blanditiis aut recusandae culpa architecto debitis
-          cum, quia natus?
-        </p>
+      <div className="my-container mt-32 grid grid-cols-1 md:grid-cols-3 justify-between items-center gap-10">
+        <div className="col-span-2">
+          <p>
+            <span className="font-bold">Job Description: </span>
+            {jobDescription}
+          </p>
+          <p className="mt-6 mb-6">
+            <span className="font-bold">Job Responsibility: </span>
+            {jobResponsibilities}
+          </p>
+          <p>
+            <span className="font-bold">Educational Requirements: </span>
+          </p>
+          <p className="mb-6 mt-4">{educationalRequirements}</p>
+          <p>
+            <span className="font-bold">Experiences: </span>
+          </p>
+          <p className="mt-4">{experience}</p>
+        </div>
+        <div>
+          <div className="bg-light-purple rounded-lg p-7">
+            <p className="text-lg font-bold">Job Details</p>
+            <hr className="mt-6" />
+            <p className="flex mt-6">
+              <img src={dollarIcon} alt="" className="mr-2" />
+              <span className="mr-2">Salary: {salary}</span> Per Month
+            </p>
+            <p className="flex mt-4">
+              <img src={jobTitleIcon} alt="" className="mr-2" />
+              <span className="mr-2">Job Title: </span>
+              {jobTitle}
+            </p>
+            <p className="text-lg font-bold mt-8">Contact Information</p>
+            <hr className="mt-6" />
+            <p className="flex mt-6">
+              <img src={phoneIcon} alt="" className="mr-2" />
+              <span className="mr-2">Phone: </span>
+              {Phone}
+            </p>
+            <p className="flex mt-4">
+              <img src={emailIcon} alt="" className="mr-2" />
+              <span className="mr-2">Email: </span>
+              {email}
+            </p>
+            <p className="flex mt-4">
+              <img src={locationImage} alt="" className="mr-2" />
+              <span className="mr-2">Address: </span>
+              {location}
+            </p>
+          </div>
+          <button className="w-full mt-6 btn-primary">Apply Now</button>
+        </div>
       </div>
     </div>
   );
